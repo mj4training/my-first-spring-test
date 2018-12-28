c6f045007bbd06c70520d095981d90289c4d49b8
mj4training <mj4training@gmail.com>
2018-12-28 15:03:06 -0500

    First commit

---
+++ src/main/java/com/ribeach/spring/rest/myfirstspringtest/MyFirstSpringTestApplication.java
@@ -0,0 +1,14 @@
+package com.ribeach.spring.rest.myfirstspringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSpringTestApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/ribeach/spring/rest/myfirstspringtest/MyFirstSpringTestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ribeach.spring.rest.myfirstspringtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyFirstSpringTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
